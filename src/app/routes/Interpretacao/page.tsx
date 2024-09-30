43351ad914725c46f1fb748dbbcfd24c1e131da1
jliamonteiro <jlmonteiro2355@gmail.com>
2024-09-30 10:47:47 -0300

    colocando informações escritas de cada página das routes e seus respectivos titulos

---
+++ src/app/routes/Interpretacao/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Interpretacao() {
+  return (
+    <div>
+        <h1 className="text-royal-blue font-arial text-2xl font-bold">Interpretação de Mitos e Representações Visuais</h1>
+        <p className='font-arial'>As imagens astronômicas podem ser comparadas com representações artísticas antigas que refletem a percepção humana do cosmos. Velikovsky sugere que muitos mitos têm raízes em eventos cósmicos reais. Analisar como essas imagens modernas se relacionam com ícones e símbolos em culturas antigas pode revelar uma continuidade na maneira como a humanidade tem interpretado o universo ao longo do tempo.</p>
+    </div>
+  )
+}
