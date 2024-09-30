0851066701413abe4acf01ae5e406a79069a3691
sofiapetruk <sofiapetruk900@gmail.com>
2024-09-30 11:43:43 -0300

    arrumação de página

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Menu() {
     return (
@@ -6,7 +6,7 @@ export default function Menu() {
             <ul className="flex space-x-4">
                 <li><Link href="/" className="text-lg font-semibold">Home</Link></li>
                 <li><Link href="/Catastrofe" className="text-lg font-semibold">Catastrofe</Link></li>
-                <li><Link href="/Interpretação" className="text-lg font-semibold">Interpretação</Link></li>
+                <li><Link href="/Interpretacao" className="text-lg font-semibold">Interpretação</Link></li>
                 <li><Link href="/Linguagem" className="text-lg font-semibold">Linguagem</Link></li>
                 <li><Link href="/" className="text-lg font-semibold">Cálculos</Link></li>
             </ul>
