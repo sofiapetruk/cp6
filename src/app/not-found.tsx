3c3c20eb19d3acc7c97b3d5b8859058167b864ea
jliamonteiro <jlmonteiro2355@gmail.com>
2024-09-30 11:09:47 -0300

    criação do not-found e inserção da imagem

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image"
+
+
+
+
 export default function NotFound() {
+
+    
     <div>
         <h1>404: Page Not Found</h1>
+        <p>Página que você está procurando não foi encontrada</p>
+        <img
+        src="/images.jpg" // Caminho da imagem (local ou URL)
+        alt="Descrição da imagem"
+        width={500} // Largura da imagem
+        height={300} />
     </div>
 }
