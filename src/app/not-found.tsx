f5477698266e7fb319dc0362813488b84f3d7a98
jliamonteiro <jlmonteiro2355@gmail.com>
2024-09-30 11:11:06 -0300

    alteração do path das routes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,12 @@
 import Image from "next/image"
 
-
-
-
 export default function NotFound() {
 
     
     <div>
         <h1>404: Page Not Found</h1>
         <p>Página que você está procurando não foi encontrada</p>
-        <img
+        <Image
         src="/images.jpg" // Caminho da imagem (local ou URL)
         alt="Descrição da imagem"
         width={500} // Largura da imagem
