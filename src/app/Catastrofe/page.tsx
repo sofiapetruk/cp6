33f8159daca267029764ec7b50560d8480fda62f
sofiapetruk <sofiapetruk900@gmail.com>
2024-09-30 15:57:13 -0300

    mudança de css

--- src/app/Catastrofe/page.tsx
+++ src/app/Catastrofe/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Catastrofe() {
   return (
     <div>
-        <h1 className="font-arial text-2xl font-bold">Catástrofes Cósmicas e Imagens de Eventos Espaciais</h1>
+        <h1 className="font-arial text-2xl font-bold mb-4">Catástrofes Cósmicas e Imagens de Eventos Espaciais</h1>
         <p className='font-arial'>As imagens da API podem mostrar fenômenos como explosões de supernovas, impactos de asteroides ou cometas. Isso se relaciona com a ideia de Velikovsky sobre catástrofes cósmicas que influenciaram a história da Terra. A visualização de tais eventos pode estimular discussões sobre como esses fenômenos podem ter impactado civilizações antigas, tanto em mitos quanto em relatos históricos.</p>
     </div>
   );
--- src/app/Interpretacao/page.tsx
+++ src/app/Interpretacao/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Interpretacao() {
   return (
     <div>
-        <h1 className="font-arial text-2xl font-bold">Interpretação de Mitos e Representações Visuais</h1>
+        <h1 className="font-arial text-2xl font-bold mb-4">Interpretação de Mitos e Representações Visuais</h1>
         <p className='font-arial'>As imagens astronômicas podem ser comparadas com representações artísticas antigas que refletem a percepção humana do cosmos. Velikovsky sugere que muitos mitos têm raízes em eventos cósmicos reais. Analisar como essas imagens modernas se relacionam com ícones e símbolos em culturas antigas pode revelar uma continuidade na maneira como a humanidade tem interpretado o universo ao longo do tempo.</p>
     </div>
   )
