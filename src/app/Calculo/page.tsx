a322039a277040652c69c771621fb8c5c52f6111
sofiapetruk <sofiapetruk900@gmail.com>
2024-09-30 16:23:15 -0300

    arrumação de imagem,

--- src/app/Calculo/page.tsx
+++ src/app/Calculo/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Calculo() {
   return (
     <div>
-        <h1>Calculo</h1>
+        <h1 className='font-arial text-2xl font-bold mb-4'>Calculo</h1>
         <Image
         src="/conta.jpg" 
         alt="Descrição da imagem"
